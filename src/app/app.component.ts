98237196192fecb3edfe9f1800bad089e1fec273
= <=>
2020-01-08 17:50:03 +0530

    edit table column data

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewChild } from '@angular/core';
+import { CSVRecord } from './CSVModel';  
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  
+}  
+
